e0e6b74fd0f38e35a5143c42bffaa4b37916fe08
Michael W <baddaemon87@gmail.com>
2024-11-15 21:01:39 +0100

    sdk: Optimize imports
    Reordering and removing unused ones
    
    Change-Id: I1fff9c6f11c71022dbf4207ee042d2ee2ff77ee6

--- lineage/lib/main/java/org/lineageos/platform/internal/health/ChargingControlController.java
+++ lineage/lib/main/java/org/lineageos/platform/internal/health/ChargingControlController.java
@@ -6,6 +6,10 @@
 package org.lineageos.platform.internal.health;
 
 import static java.time.format.FormatStyle.SHORT;
+import static lineageos.health.HealthInterface.MODE_AUTO;
+import static lineageos.health.HealthInterface.MODE_LIMIT;
+import static lineageos.health.HealthInterface.MODE_MANUAL;
+import static lineageos.health.HealthInterface.MODE_NONE;
 
 import android.app.AlarmManager;
 import android.app.Notification;
@@ -29,6 +33,8 @@ import android.util.Log;
 
 import org.lineageos.platform.internal.R;
 
+import lineageos.providers.LineageSettings;
+
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
@@ -40,15 +46,8 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
-import lineageos.providers.LineageSettings;
-
 import vendor.lineage.health.ChargingControlSupportedMode;
 import vendor.lineage.health.IChargingControl;
-
-import static lineageos.health.HealthInterface.MODE_NONE;
-import static lineageos.health.HealthInterface.MODE_AUTO;
-import static lineageos.health.HealthInterface.MODE_MANUAL;
-import static lineageos.health.HealthInterface.MODE_LIMIT;
 
 public class ChargingControlController extends LineageHealthFeature {
     private final IChargingControl mChargingControl;
